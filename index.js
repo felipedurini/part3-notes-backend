58a9c98687912602124787d1efea321f18785d4f
felipeuba <ubafelipe005@gmail.com>
2025-02-11 11:21:11 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const Note = require('./models/note')
-const mongoose = require('mongoose')
+const app = require('./app') 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-require('dotenv').config()
-
-const password = process.argv[2]
-
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.log('Error connecting to MongoDB:', error.message))
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => response.json(notes))
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
